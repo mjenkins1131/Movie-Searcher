017417fc6ffd24c5a303b9f5ba141d50c398bf3d
Marvin <mjenkins1131@yahoo.com>
2022-04-17 20:37:27 -0400

    Complete Project Commit

---
+++ MovieSearchService/MovieSearchService/Controllers/SearchMovie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MovieSearchService.Models;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieSearchService.Controllers
+{
+    [Route("api/SearchMovie")]
+    [ApiController]
+    public class SearchMovie : ControllerBase
+    {
+        // GET api/<SearchMovie>/Lilo
+        [HttpGet("{name}")]
+        public async Task<List<string>> GetAsync(string name)
+        {
+            List<Movie> movieNameList = new List<Movie>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"https://api.themoviedb.org/3/search/movie?api_key=4544d8b9aba2eabf203d74f96d505707&query={name}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    DatabaseAPIResponse responseObject = JsonConvert.DeserializeObject<DatabaseAPIResponse>(apiResponse);
+                    movieNameList = responseObject.Results;
+                }
+            }
+
+            return movieNameList.Count() > 1 ? movieNameList.Select(p => p.Title).ToList() : new List<string>() { "No Movie Found!"};
+        }
+    }
+}
---
+++ MovieSearchService/MovieSearchService/Models/DatabaseAPIResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSearchService.Models
+{
+    public class DatabaseAPIResponse
+    {
+        public int page { get; set; }
+        public List<Movie> Results { get; set; }
+        public int Total_Pages { get; set; }
+        public int Total_Results { get; set; }
+    }
+}
